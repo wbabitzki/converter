6c900d97883eeb05a37f618f312b71754f899b44
Wladimir Babitzki <wbabitzki@gmail.com>
2019-02-12 21:57:23 +0100

    added custom LocalDate deserializer

--- b2s-rest/src/main/java/ch/wba/accounting/rest/JacksonObjectMapperProvider.java
+++ b2s-rest/src/main/java/ch/wba/accounting/rest/JacksonObjectMapperProvider.java
@@ -1,25 +1,21 @@
 package ch.wba.accounting.rest;
 
-import java.io.IOException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
-import javax.ws.rs.ext.ContextResolver;
-
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import javax.ws.rs.ext.ContextResolver;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class JacksonObjectMapperProvider implements ContextResolver<ObjectMapper> {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    final ObjectMapper defaultObjectMapper;
+    private final ObjectMapper defaultObjectMapper;
 
     public JacksonObjectMapperProvider() {
         defaultObjectMapper = createDefaultMapper();
@@ -33,10 +29,22 @@ public class JacksonObjectMapperProvider implements ContextResolver<ObjectMapper
     private static ObjectMapper createDefaultMapper() {
         final ObjectMapper mapper = new ObjectMapper();
         final JavaTimeModule javaTimeModule = new JavaTimeModule();
-        javaTimeModule.addSerializer(LocalDate.class, new JsonSerializer<LocalDate>() {
+        javaTimeModule //
+        .addSerializer(LocalDate.class, new JsonSerializer<LocalDate>() {
             @Override
-            public void serialize(final LocalDate value, final JsonGenerator gen, final SerializerProvider serializers) throws IOException, JsonProcessingException {
+            public void serialize(final LocalDate value, final JsonGenerator gen, final SerializerProvider serializers) throws IOException {
                 gen.writeString(value.format(FORMATTER));
+            }
+        }) //
+        .addDeserializer(LocalDate.class, new JsonDeserializer<LocalDate>() {
+            @Override
+            public LocalDate deserialize(final JsonParser p, final DeserializationContext ctx) throws IOException {
+                try {
+                    return LocalDate.parse(p.getValueAsString(), FORMATTER);
+                } catch (final DateTimeParseException e) {
+                    System.out.format("Invalid date: '%s', not confirm to the format '%s'", p.getValueAsString(), FORMATTER);
+                    return null;
+                }
             }
         });
         mapper.registerModule(javaTimeModule);
